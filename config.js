41416d27013e6306100c1dda550c0cf9b6b2ce69
Saranraj <saran@atatus.com>
2021-08-16 11:14:00 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+const str = {
+  "connectionString": "mongodb+srv://interview:welcome2byteridge@byteridge-hdrl6.mongodb.net/interview",
+  "secret": "JBJYBJ23jkj232kALKMLAK"
+  // "connectionString": "mongodb://localhost:27017/audit"
+}
+
+module.exports = str;
